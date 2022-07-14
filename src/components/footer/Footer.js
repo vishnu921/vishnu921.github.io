b07bad9d7fe1746e5db4f40b26aaaf9d1450f669
vishnu0921 <ba11@iitbbs.ac.in>
2022-07-14 19:52:48 +0530

    Added footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin, BsInstagram} from 'react-icons/bs'
+import {FaGithub, FaTwitter} from 'react-icons/fa'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="footer-copyright">
+        <small>&copy; 2022 Bishnu Amola. All rights resrved.</small>
+      </div>
+
+      <div className="footer-socials">
+        <a href="https://www.linkedin.com/in/bishnu-amola/" target="_blank" rel="noreferrer"><BsLinkedin /></a>
+        <a href="https://www.twitter.com/AmolaVishnu/" target="_blank" rel="noreferrer"><FaTwitter /></a>
+        <a href="https://www.github.com/vishnu921" target="_blank" rel="noreferrer"><FaGithub /></a>
+        <a href="https://www.instagram.com/" target="_blank" rel="noreferrer"><BsInstagram /></a>
+      </div>
+    </footer>
   )
 }
 
