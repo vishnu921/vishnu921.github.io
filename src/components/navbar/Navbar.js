d78984272b13cab504249f5dc7b6bc9b55dd136c
vishnu0921 <ba11@iitbbs.ac.in>
2022-07-16 19:50:45 +0530

    refractored navbar and btn

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,29 +1,35 @@
 import React from 'react'
 import './navbar.css'
 import {NavLink} from 'react-router-dom'
-import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai'
-import {FaCode, FaPaperPlane, FaLaptopCode} from 'react-icons/fa'
+import {FaHome, FaCode, FaPaperPlane, FaLaptopCode, FaUser} from 'react-icons/fa'
 
 function Navbar() {
 
   return (
-    <nav>
-      <NavLink to="/">
-        <AiOutlineHome />
-      </NavLink>
-      <NavLink to="about">
-        <AiOutlineUser />
-      </NavLink>
-      <NavLink to="skills">
-        <FaCode />
-      </NavLink>
-      <NavLink to="projects">
-        <FaLaptopCode />
-      </NavLink>
-      <NavLink to="contact">
-        <FaPaperPlane />
-      </NavLink>
-    </nav>
+    <header>
+      <nav>
+        <NavLink to="/">
+          <FaHome className='nav-icon'/>
+          <span className='nav-item-name'>Home</span>
+        </NavLink>
+        <NavLink to="about">
+          <FaUser className='nav-icon'/>
+          <span className='nav-item-name'>About</span>
+        </NavLink>
+        <NavLink to="skills">
+          <FaCode className='nav-icon'/>
+          <span className='nav-item-name'>Skills</span>
+        </NavLink>
+        <NavLink to="projects">
+          <FaLaptopCode className='nav-icon'/>
+          <span className='nav-item-name'>Projects</span>
+        </NavLink>
+        <NavLink to="contact">
+          <FaPaperPlane className='nav-icon'/>
+          <span className='nav-item-name'>Contact</span>
+        </NavLink>
+      </nav>
+    </header>
   )
 }
 
